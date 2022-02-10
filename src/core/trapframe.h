a39867dbf90868d7c0e6fd579bba19d69b2e3163
chenld <19307130181@fudan.edu.cn>
2022-02-10 12:04:46 +0800

    a NOTE

--- src/core/trapframe.h
+++ src/core/trapframe.h
@@ -13,6 +13,7 @@
  */
 typedef struct {
 	/* DONE: Lab3 Interrupt */
+	// NOTE: TPIDR_EL0 is necessary for multi core !
 	u64 TPIDR_EL0,ELR_EL1,SPSR_EL1,SP_EL0;
     u64 x30,x29,x28,x27,x26,x25,x24,x23,x22,x21,x20,x19,x18,x17,
 		x16,x15,x14,x13,x12,x11,x10,x9,x8,x7,x6,x5,x4,x3,x2,x1,x0;
